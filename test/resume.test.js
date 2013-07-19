a38575fa1a9b7262e2958ca5422f8305cd473cdf
Lautaro <lautaromail@gmail.com>
2013-07-19 12:27:49 +0200

    fix test

--- test/resume.test.js
+++ test/resume.test.js
@@ -17,13 +17,16 @@ exports.testFindCube = function(test) {
 			console.log(value);
 			console.log("fin value");
 			console.log(new Date(2013,6,1).toUTCString());
+			console.log(new Date(2013,6,1).getTimezoneOffset());
+			console.log(new Date(2013,6,17).toUTCString());
+			console.log(new Date(2013,6,17).getTimezoneOffset());
 			test.equal(value[new Date(2013,6,1).toUTCString()].caixa,9.9,'caixa dia 1');
 			test.equal(value[new Date(2013,6,1).toUTCString()].bonus,-3.9,'bonus dia 1');
 			test.equal(value[new Date(2013,6,1).toUTCString()].cash,3.19,'cash dia 1');
 			test.equal(value[new Date(2013,6,1).toUTCString()]['super'],-3.19,'super dia 1');
 			test.equal(value[new Date(2013,6,1).toUTCString()].vicio,-6,'vicio dia 1');
 
-			console.log(new Date(2013,6,17).toUTCString());
+			
 			test.equal(value[new Date(2013,6,17).toUTCString()].caixa,46.150000000000006,'caixa dia 17');
 			test.equal(value[new Date(2013,6,17).toUTCString()].deudas_general,-0.3000000000000007,'deudas dia 17');
 			test.equal(value[new Date(2013,6,17).toUTCString()].cash,-19,'cash dia 17');
