d8062cc9e0f41e86068b8f6571fb2dde188a4bd9
lautaro <lautaromail@gmail.com>
2013-08-07 20:49:09 +0200

    addBonus.html first version

--- public/js/addBonus.js
+++ public/js/addBonus.js
@@ -5,21 +5,36 @@ var bonus = angular.module('cashcube.quick.bonus',['ngResource','ui.bootstrap'])
 bonus.controller("AddBonusController", function($scope,Movement,$timeout) {
 	$scope.loading = 0;
 
+    $scope.template = 'form.html';
+
 	//Generalize, create a directive with menu
     $scope.navigateTo = function(href) {
         window.location.href = href;
     };
 
 	$scope.item = {
-		date: new Date(),
-		account: 'cash',
-		accountTarget: 'bonus',
-		accountCurrency:1,
-		accountTargetCurrency:1
+        date: new Date().toJSON().slice(0,10),
+        account:'cash',
+        accountTarget: 'bonus',
+        tags: '',
+        accountCurrency:1,
+        accountTargetCurrency:1
 	};
 
 	$scope.save = function() {
-		Movement.save($scope.item);
+        $scope.error = null;
+        var date = new Date();
+        date.setYear(parseInt($scope.item.date.split('-')[0]));
+        date.setMonth(parseInt($scope.item.date.split('-')[1])-1);
+        date.setDate(parseInt($scope.item.date.split('-')[2]));
+        date.setMilliseconds(0);
+        date.setSeconds(0);
+        date.setMinutes(0);
+        date.setHours(0);
+        $scope.item.date = date;
+        var result = Movement.save($scope.item, function() {
+            $scope.template = 'success.html';
+        });
 	};
 
 });
