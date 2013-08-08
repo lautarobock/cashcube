62d5e43fec756643cb76fbe2f107ec6fbda757ed
Lautaro <lautaromail@gmail.com>
2013-08-08 13:26:56 +0200

    fix calendar picker

--- public/js/addBonus.js
+++ public/js/addBonus.js
@@ -5,6 +5,8 @@ var bonus = angular.module('cashcube.quick.bonus',['ngResource','ui.bootstrap'])
 bonus.controller("AddBonusController", function($scope,Movement,$timeout) {
 	$scope.loading = 0;
 
+	$scope.availableAccount = ['cash','caixa','tarjeta'];
+	
     $scope.template = 'form.html';
 
 	//Generalize, create a directive with menu
@@ -22,25 +24,21 @@ bonus.controller("AddBonusController", function($scope,Movement,$timeout) {
 	};
 
 	$scope.save = function() {
-//        $scope.error = null;
-//        var date = new Date();
-//        date.setYear(parseInt($scope.item.date.split('-')[0]));
-//        date.setMonth(parseInt($scope.item.date.split('-')[1])-1);
-//        date.setDate(parseInt($scope.item.date.split('-')[2]));
-//        date.setMilliseconds(0);
-//        date.setSeconds(0);
-//        date.setMinutes(0);
-//        date.setHours(0);
-//        $scope.item.date = date;
+		$scope.loading = 1;
         $scope.item.date.setMilliseconds(0);
         $scope.item.date.setSeconds(0);
         $scope.item.date.setMinutes(0);
         $scope.item.date.setHours(0)
         var result = Movement.save($scope.item, function() {
             $scope.template = 'success.html';
+			$scope.loading = 0;
         });
 	};
 
+	$scope.showDatePicker = false;
+	$scope.open = function() {
+		$scope.showDatePicker = !$scope.showDatePicker;
+	};
 });
 
 
